9201dd776b225e45302d1651fb059d7e500e587b
hero2828 <719857499@qq.com>
2025-07-20 15:20:15 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
-import { Client, Users, Databases,ID } from 'node-appwrite';
+import { Client, Databases, ID, Users } from 'node-appwrite'
 
 export default async ({ req, res, log, error }) => {
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(process.env.key);
-  const users = new Users(client);
-  const db=new Databases(client)
-  const promise= db.createDocument(
+    .setKey(process.env.key)
+  const users = new Users(client)
+  const db = new Databases(client)
+  const promise = db.createDocument(
     process.env.db,
     process.env.collection,
     ID.unique(),
     {
-      name:new Date().toLocaleString(),
-    }
+      name: new Date().toLocaleString(),
+    },
   )
-  const data = await promise;
-  return res.send({ message: data, env: process.env }，200，{
+  const data = await promise
+  return res.send({ message: data, env: process.env }, 200, {
     'Access-Control-Allow-Origin': '*',
-  });
+  })
 }
